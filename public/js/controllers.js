474a117d7734553752a5dab59bda7bc7f5ed66bc
Daniel <danielhernandez16@hotmail.com>
2015-03-02 05:28:52 -0500

    Data binding

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,3 +1,32 @@
 'use strict';
 
 /* Controllers */
+
+
+(function() {
+	// 2 param es el arreglo de las dependencias 
+	angular.module('prueba.controllers', []) //sin  ;  al final para tener chainmethods
+
+		.controller('BooksController', ['$scope', 'pruebaService', function ($scope, pruebaService){
+
+			pruebaService.all().then(function (data){
+				console.log(data);
+				$scope.books = data;
+				$scope.tyu = 5;
+			});
+		}])
+		
+
+		// .controller('ProductController', ['$scope', '$routeParams', 'kinggraficService', function ($scope, $routeParams, kinggraficService) {
+		// 	var name = $routeParams.name;
+
+		// 	kinggraficService.byName(name)
+		// 		.then(function (data){
+		// 			$scope.product = data;
+		// 		})
+		// }])
+
+
+		
+
+})();
